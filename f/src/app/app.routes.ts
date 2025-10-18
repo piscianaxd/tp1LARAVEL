039ddec628855507c7a4e68144593754c5ce9fd1
juandefuego <juanifederik@gmail.com>
2025-10-17 21:21:00 -0300

    Añadir componente de inicio de sesión y servicio de autenticación; actualizar dependencias y rutas

--- f/src/app/app.routes.ts
+++ f/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { PlaylistComponent } from './pages/playlist/playlist.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
   { path: '', component: PlaylistComponent }, // Ruta principal
   { path: 'playlists', component: PlaylistComponent },
   { path: 'playlist', redirectTo: 'playlists', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
 ];
---
+++ f/src/app/pages/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+  errorMessage = '';
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) return;
+    this.authService.login(this.loginForm.value).subscribe({
+      next: () => this.router.navigate(['/']),
+      error: err => this.errorMessage = err.error?.message || 'Error al iniciar sesión'
+    });
+  }
+}
